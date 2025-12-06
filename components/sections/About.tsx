55a1b5aa691f20778f48dd4b8f4a1023d6417750
Aryan Chhetri <aryanjr53@gmail.com>
2025-12-06 13:23:01 +0530

    fix - icon

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -99,6 +99,7 @@ export default function About() {
               <CardContent className='p-6'>
                 <div className='flex items-center space-x-3 mb-4'>
                   <div className='p-2 bg-purple-100 dark:bg-purple-900/20 rounded-lg'>
+                    <Zap className='w-5 h-5 text-purple-600' />
                   </div>
                   <h4 className='font-semibold text-foreground'>
                     Continuous Learning
